3aac3c548b0e551bdfde5c552a70f1c85b2aada1
tawhidib <tawhidib@gmail.com>
2023-03-19 22:02:54 +0600

    style: Tailwind install and setup
    install tailwind npm and setup as documentation for nextjs

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SignIn = (): JSX.Element => {
-  return <>sign in page</>;
+  return <h2 className="text-teal-500">sign in page</h2>;
 };
 
 export default SignIn;
