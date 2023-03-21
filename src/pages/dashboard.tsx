d0ccc9b04914eeb281873f6bc0d0be9220f4b507
tawhidib <tawhidib@gmail.com>
2023-03-21 18:10:44 +0600

    feat: layout giving to private route

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,8 @@
+import Layout from "@/components/layout";
+import React from "react";
+
+const Dashboard = () => {
+  return <h3></h3>;
+};
+Dashboard.getLayout = (page) => <Layout>{page}</Layout>;
+export default Dashboard;
