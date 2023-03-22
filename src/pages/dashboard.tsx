ef2b64b804aa83019409bec7e4a09ea441dc0422
tawhidib <tawhidib@gmail.com>
2023-03-22 16:55:33 +0600

    feat: dashboard reroute to sign in page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
 import Layout from "@/components/layout";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
 const Dashboard = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      router.push("/sign-in");
+    }
+  });
   return (
     <div className="">
       <h3>This is dashboard...this one is private route</h3>
